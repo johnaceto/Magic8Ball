311e1798fee4c4f2b41e5e65b94f6c3d8a5b97bc
johnaceto <john.aceto@rutgers.edu>
2018-09-26 20:50:04 -0400

    Initial Commit

---
+++ MagicEightBall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagicEightBall
+//
+//  Created by John Aceto on 9/26/18.
+//  Copyright © 2018 John Aceto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
